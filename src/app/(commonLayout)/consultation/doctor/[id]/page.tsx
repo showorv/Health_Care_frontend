84f5c80521c0ece4da0feb6bc905adabb4516ffd
showorv <yshowrov7@gmail.com>
2026-09-20 22:15:02 +0600

    axios and tanstack setup

---
+++ src/app/(commonLayout)/consultation/doctor/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConsultationDoctorById = () => {
+  return (
+    <div>ConsultationDoctorById</div>
+  )
+}
+
+export default ConsultationDoctorById
---
+++ src/app/(dashboardLayout)/admin/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminDasbboardPage = () => {
+  return (
+    <div>AdminDasbboardPage</div>
+  )
+}
+
+export default AdminDasbboardPage
